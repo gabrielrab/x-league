7bf30d63c193adfa050363fd84cea1844af3fa5a
Gabriel Rabelo Camargos <rabelogabriel72@gmail.com>
2019-11-08 09:30:38 -0300

    Initial commit

---
+++ src/pages/Index.js
@@ -0,0 +1,25 @@
+import React from "react";
+//import { Link } from "react-router-dom";
+
+import "./index.css";
+
+export default function() {
+  return (
+    <>
+      <nav>
+        <div>X</div>
+        <ul>
+          <li>Campeonatos</li>
+          <li>Transmissoes</li>
+          <li>Perfil</li>
+          <li>Equipe</li>
+          <li>Aulas</li>
+          <li>Premium</li>
+        </ul>
+        <div className="profile-content">
+          <img src="" alt="Profile" />
+        </div>
+      </nav>
+    </>
+  );
+}
